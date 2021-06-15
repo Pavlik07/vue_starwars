c041c38e4033e00ecaba592dca1d433f115cf2f0
Pavel Mosk <64908466+Pavlik07@users.noreply.github.com>
2021-06-15 12:48:10 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <h1>Star Wars Characters</h1>
+      <div class="cards">
+        <HumanCard 
+          v-for='human in people.results' 
+          :key='human.name'
+          :human='human'
+        />
+      </div>
+  </div>
+</template>
+
+<script>
+import HumanCard from './components/HumanCard'
+export default {
+  name: 'App',
+  components: {
+    HumanCard,
+  },
+  data() {
+    return {
+      people: {},
+    }
+  },
+  mounted() {
+    fetch('https://swapi.dev/api/people')
+      .then(res => res.json())
+      .then(data => this.people = data)
+      .catch(err => console.log(err.message));
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+.cards>* {
+  flex: 400px;
+  width: fit-content
+}
+</style>
